e9f937125f119a5017654846a9ba9573a8321f6a
Mike Rocke <mikerocke1989@gmail.com>
2025-07-19 20:09:46 +0100

    chapter 2 done web server setup

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    './src/index02_hello.ts'
+  ],
+  // output: {
+  //   path: __dirname + '/dist',
+  //   filename: "app.bundle.js"
+  // },
+  resolve: {
+    extensions: [".js", ".ts"],
+    modules: ['src', 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader"
+        }
+      },
+      {
+        test: /\.(ico)$/,
+        use: {
+          loader: "file-loader?name=src/[name].[ext]"
+        }
+      }
+    ]
+  },
+  mode: 'development',
+  devServer: { static: './dist' },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './src/index0.html',
+        favicon: './src/favicon.ico'
+      }
+    ),
+    new webpack.ProvidePlugin(
+      { $: 'jquery', jquery: 'jquery' }
+    )
+  ]
+};
